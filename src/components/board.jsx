36ec47c07907ec616cf27df770929cb3f81709e5
cc13446 <1344613920@qq.com>
2023-10-02 22:34:42 +0800

    move left

--- src/components/board.jsx
+++ src/components/board.jsx
@@ -1,22 +1,50 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Board from "../core";
-import Cell from "./cell"
+import Cell from "./cell";
 import Message from "./message";
 
 const BoardView = () => {
   const [board, setBoard] = useState(new Board());
 
   const cells = board.cells.map((n, index) => {
-    return (<Cell key={index} number={n} />);
+    return <Cell key={index} number={n} />;
   });
 
   const copyScoreToClipboard = () => {
     navigator.clipboard.writeText("Your best score is " + board.score);
-  }
+  };
 
   const resetGame = () => {
     setBoard(new Board());
   };
+
+  const handleKeyDown = (event) => {
+    if (board.finish) {
+      return;
+    }
+
+    if (event.keyCode >= 37 && event.keyCode <= 40) {
+      let direction = event.keyCode - 37;
+      let newBoard = Object.assign(
+        Object.create(Object.getPrototypeOf(board)),
+        board
+      );
+      newBoard.moveCel(direction);
+      setBoard(newBoard);
+    }
+  };
+
+  const useEvent = (event, handler, passive = false) => {
+    useEffect(() => {
+      window.addEventListener(event, handler, passive);
+
+      return function cleanUp() {
+        window.removeEventListener(event, handler, passive);
+      };
+    });
+  };
+
+  useEvent("keydown", handleKeyDown);
 
   return (
     <div className="w-[440px] h-[600px] flex flex-col justify-start items-center">
--- src/core/index.js
+++ src/core/index.js
@@ -1,10 +1,69 @@
 class Board {
-    constructor() {
-        this.score = 0;
-        this.finish = false;
-        this.size = 4;
-        this.cells = new Array(this.size * this.size).fill(1024);
+  constructor() {
+    this.score = 0;
+    this.finish = false;
+    this.size = 4;
+    this.cells = new Array(this.size * this.size).fill(undefined);
+
+    this.deltaX = [-1, 0, 1, 0];
+    this.deltaY = [0, -1, 0, 1];
+
+    this.addRandomCell();
+    this.addRandomCell();
+  }
+
+  addRandomCell() {
+    let emptyCells = [];
+    for (let i = 0; i < this.size * this.size; i++) {
+      if (this.cells[i] === undefined) {
+        emptyCells.push(i);
+      }
     }
+    let index = ~~(Math.random() * emptyCells.length);
+    let cell = emptyCells[index];
+    let newValue = Math.random() > 0.75 ? 4 : 2;
+    this.cells[cell] = newValue;
+  }
+
+  moveCellTo(from, to) {
+    let max = this.size * this.size;
+    if (from >= max || from < 0 || to >= max || to < 0) {
+        return false;
+    }
+    if (this.cells[to] === undefined) {
+      this.cells[to] = this.cells[from];
+      this.cells[from] = undefined;
+      return true;
+    }
+    return false;
+  }
+
+  moveCel(direction) {
+    // 0 -> left, 1 -> up, 2 -> right, 3 -> down
+    let moveFrom = [
+        (i, j) => i * this.size + j,
+    ];
+    let moveTo = [
+        (from) => from % this.size === 0 ? -1 : from - 1
+    ];
+
+    for (let i = 0; i < this.size; i++) {
+      // move
+      for (let j = 0; j < this.size; j++) {
+        let index = moveFrom[direction](i, j);
+        let to = moveTo[direction](index);
+        while(this.moveCellTo(index, to)) {
+            index = to;
+            to = moveTo[direction](index);
+        }
+      }
+      // merge
+      for (let j = 0; j < this.size; j++) {}
+      //move again
+      for (let j = 0; j < this.size; j++) {}
+    }
+
+  }
 }
 
 export default Board;
