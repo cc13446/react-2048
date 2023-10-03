d7a7985f34e7321b86f8cc44e40648a8829eaef8
cc13446 <1344613920@qq.com>
2023-10-03 16:39:22 +0800

    适配移动端

--- src/components/board.jsx
+++ src/components/board.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useCallback } from "react";
 import Board from "../core";
 import Cell from "./cell";
 import Message from "./message";
@@ -18,13 +18,8 @@ const BoardView = () => {
         setBoard(new Board());
     };
 
-    const handleKeyDown = (event) => {
-        if (board.finish) {
-            return;
-        }
-
-        if (event.keyCode >= 37 && event.keyCode <= 40) {
-            let direction = event.keyCode - 37;
+    const move = useCallback(
+        (direction) => {
             let newBoard = Object.assign(
                 Object.create(Object.getPrototypeOf(board)),
                 board
@@ -33,6 +28,18 @@ const BoardView = () => {
             newBoard.addRandomCell();
             newBoard.freshFinish();
             setBoard(newBoard);
+        },
+        [board, setBoard]
+    );
+
+    const handleKeyDown = (event) => {
+        if (board.finish) {
+            return;
+        }
+
+        if (event.keyCode >= 37 && event.keyCode <= 40) {
+            let direction = event.keyCode - 37;
+            move(direction);
         }
     };
 
@@ -48,6 +55,44 @@ const BoardView = () => {
 
     useEvent("keydown", handleKeyDown);
 
+    useEffect(() => {
+        let firstX = 0;
+        let firstY = 0;
+
+        const touchStart = (event) => {
+            event.stopPropagation();
+            event.preventDefault();
+            firstX = event.touches[0].clientX;
+            firstY = event.touches[0].clientY;
+        };
+
+        const touchEnd = (event) => {
+            event.stopPropagation();
+            event.preventDefault();
+            console.log(event);
+            let moveX = event.changedTouches[0].clientX - firstX;
+            let moveY = event.changedTouches[0].clientY - firstY;
+            if (Math.abs(moveX) > 100 || Math.abs(moveY) > 100) {
+                if (Math.abs(moveX) > Math.abs(moveY)) {
+                    let direction = moveX > 0 ? 2 : 0;
+                    move(direction);
+                } else {
+                    let direction = moveY < 0 ? 1 : 3;
+                    move(direction);
+                }
+            }
+        };
+
+        let b = document.getElementById("board");
+        b.addEventListener("touchstart", touchStart, { passive: false });
+        b.addEventListener("touchend", touchEnd, { passive: false });
+
+        return function cleanUp() {
+            b.removeEventListener("touchstart", touchStart, { passive: false });
+            b.removeEventListener("touchend", touchEnd, { passive: false });
+        };
+    }, [move]);
+
     return (
         <div className="w-[440px] h-[600px] flex flex-col justify-start items-center">
             <div className="w-[420px] mb-5 flex justify-between items-center">
@@ -57,7 +102,10 @@ const BoardView = () => {
                     onClick={copyScoreToClipboard}
                 />
             </div>
-            <div className="w-[440px] h-[440px] p-[5px] mb-5 bg-teal-700 flex flex-wrap justify-around items-center rounded-xl">
+            <div
+                id="board"
+                className="w-[440px] h-[440px] p-[5px] mb-5 bg-teal-700 flex flex-wrap justify-around items-center rounded-xl"
+            >
                 {cells}
             </div>
             <div hidden={!board.finish}>
